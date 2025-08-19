6121abd801353a3837ba705c514c194fa2c0805c
muriloMiranda404 <murilomiranda404@gmail.com>
2025-08-19 20:24:27 -0300

    atualização

--- src/main/java/frc/robot/subsystems/teste.java
+++ src/main/java/frc/robot/subsystems/teste.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.LoggedRobot;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.Pigeon2;
 import frc.robot.subsystems.controllers.DriverController;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
@@ -14,11 +16,26 @@ public class teste extends LoggedRobot{
     LimelightConfig limelightConfig;
     DriverController driverController;
 
+    CANcoder FrontRight;
+    CANcoder FrontLeft;
+    CANcoder BackRight;
+    CANcoder BackLeft;
+
+    Pigeon2 pigeon;
+
     public teste(){
         this.driverController = DriverController.getInstance();
         this.elevatorSubsystem = ElevatorSubsystem.getInstance();
         this.intakeSubsystem = IntakeSubsystem.getInstance();
         this.swerveSubsystem = SwerveSubsystem.getInstance();
+        this.limelightConfig = LimelightConfig.getInstance();
+
+        FrontLeft = new CANcoder(10);
+        FrontRight = new CANcoder(11);
+        BackLeft = new CANcoder(13);
+        BackRight = new CANcoder(12);
+
+        pigeon = new Pigeon2(9);
     }
 
     @AutoLogOutput
@@ -49,5 +66,40 @@ public class teste extends LoggedRobot{
     @AutoLogOutput
     public double getRightElevatorOutput(){
         return elevatorSubsystem.getOutputInElevatorMotors()[0];
+    }
+
+    @AutoLogOutput
+    public double getFrontLeft(){
+        return FrontLeft.getPosition().getValueAsDouble();
+    }
+
+    @AutoLogOutput
+    public double getFrontRight(){
+        return FrontRight.getPosition().getValueAsDouble();
+    }
+
+    @AutoLogOutput
+    public double getBackLeft(){
+        return BackLeft.getPosition().getValueAsDouble();
+    }
+    
+    @AutoLogOutput
+    public double getBackRight(){
+        return BackRight.getPosition().getValueAsDouble();
+    }
+
+    @AutoLogOutput
+    public double getGyroscopyYaw(){
+        return pigeon.getYaw().getValueAsDouble();
+    }
+
+    @AutoLogOutput
+    public double getTxSetpoint(){
+        return limelightConfig.getAprilTagCordenates()[0];
+    }
+
+    @AutoLogOutput
+    public double getTySetpoint(){
+        return limelightConfig.getAprilTagCordenates()[1];
     }
 }
